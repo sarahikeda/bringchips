4738ca73d5e168a81592af7311c95fb6e162840a
Sarah Ing <sarahikeda@gmail.com>
2015-05-02 11:13:20 -0500

    fixed user form so name is incorporated on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
   end
 end
