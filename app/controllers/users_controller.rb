219eb624d04b2bcf5a9904de8a012e810e2c6d5f
Sarah Ing <sarahikeda@gmail.com>
2016-02-28 11:59:09 -0600

    Refactor to check user has events before adding dish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,16 +17,12 @@ class UsersController < ApplicationController
   def show
     @dishes = current_user.dishes.order(created_at: :desc).limit(5)
     @created_events = current_user.created_events
+    @created_and_attending = current_user.events + current_user.created_events
   end
 
   private
-  def user_params
-    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
-  end
-  def all_events
-    @created_and_attending = []
-    @created_and_attending << current_user.events
-    @created_and_attending << current_user.created_events
-    @created_and_attending
-  end
+    def user_params
+      params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
+    end
+
 end
